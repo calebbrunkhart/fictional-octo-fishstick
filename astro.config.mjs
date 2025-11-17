10cc7b15ca9806f6253d2703da010cbe5fd9dbbc
calebbrunkhart <157050203+calebbrunkhart@users.noreply.github.com>
2025-11-17 11:23:50 -0700

    Add MDX integration and set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  integrations: [mdx()],
   site: 'https://calebbrunkhart.github.io',
   base: '/fictional-octo-fishstick',
+  output: 'static'
 });
