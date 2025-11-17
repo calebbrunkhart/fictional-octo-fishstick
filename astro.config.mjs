5a5ccfec5d88ecadeba381e25b3cb938682ceb3c
calebbrunkhart <157050203+calebbrunkhart@users.noreply.github.com>
2025-11-17 11:09:15 -0700

    Update Astro config for new base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [mdx()],
-  site: 'https://calebbrunkhart.github.io', // change if you deploy to a different domain
-  output: 'static'
+  site: 'https://calebbrunkhart.github.io',
+  base: '/fictional-octo-fishstick',
 });
